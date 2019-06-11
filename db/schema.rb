e4f70eb31e95741a3e9bcc79ce691a6d5d90d966
Vince Carollo <vincecarollo@gmail.com>
2019-06-11 17:37:54 -0600

    Add description attribute to articles table

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 20190611230033) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.string "description"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
