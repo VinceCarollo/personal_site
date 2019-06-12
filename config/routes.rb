63c0bd7b8871712f88ea1dfed19ff25296c21ad5
Vince Carollo <vincecarollo@gmail.com>
2019-06-12 08:39:13 -0600

    Add routes for logging in and creating a new user

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   root 'welcome#home'
 
   get '/mystory', to: 'welcome#story'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#login'
+
   resources :articles, only: [:index]
+
+  resources :users, only: [:new, :create]
 end
