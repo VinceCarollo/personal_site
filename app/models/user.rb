39e24690bc836909dc692ace59991a52f440da61
Vince Carollo <vincecarollo@gmail.com>
2019-06-12 08:40:11 -0600

    Change username validation wording in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates_presence_of :username, uniqueness: true
+  validates :username, presence: true, uniqueness: true
   validates_presence_of :email
   validates_presence_of :password_digest, require: true
   has_secure_password
