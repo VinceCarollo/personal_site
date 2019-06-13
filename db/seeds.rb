3d50ce76e7100dd4cd3bab5087e83741448e7fdc
Vince Carollo <vincecarollo@gmail.com>
2019-06-13 08:55:01 -0600

    Add image and medium link to articles table and remove body attribute

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.destroy_all
+
+Article.create!(title: 'Down Rails', description: "The Story of my Jump into Ruby on Rails", medium_link: 'https://medium.com/@vincecarollo/down-rails-4d62f19bfc03', image: 'https://cdn-images-1.medium.com/max/1440/1*WzNe6SVM5_qi8J0t-wrqYA.png')
