e8304d227e6ea1ca3a89c9685f71b55e50890606
Vince Carollo <vincecarollo@gmail.com>
2019-06-13 08:54:36 -0600

    Change article model and spec to not validate body

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,3 @@
-class Article < ApplicationRecord 
-    validates_presence_of :title, :body
+class Article < ApplicationRecord
+    validates_presence_of :title, :description
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Article, type: :model do 
+RSpec.describe Article, type: :model do
     it { should validate_presence_of :title }
-    it { should validate_presence_of :body }
+    it { should validate_presence_of :description }
 end
